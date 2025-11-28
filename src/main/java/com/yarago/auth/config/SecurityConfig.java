2d000d6de94febbabb6a6d85eaf22b40f7a677fc
sreeramsuresh <sreeramsuresh@gmail.com>
2025-11-28 12:51:50 +0530

    bugs fixed

--- src/main/java/com/yarago/auth/config/SecurityConfig.java
+++ src/main/java/com/yarago/auth/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3080", "http://localhost:5173", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
